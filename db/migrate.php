fc8b6bc89f2a9a7df1effd6d894adf4ffa53f5b4
Simeon Naydenov <moniNaydenov@gmail.com>
2017-11-21 14:29:43 +0200

    move migrate.php to db/migrate.php

--- db/migrate.php
+++ db/migrate.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once(__DIR__ . '/../../config.php');
-require_once(__DIR__ . '/locallib.php');
+require_once(__DIR__ . '/../../../config.php');
+require_once(__DIR__ . '/../locallib.php');
 
 require_login();
 
